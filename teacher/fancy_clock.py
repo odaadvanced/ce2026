c4b5a8dff75f0723de493bfd9e8367b561bb9288
odaadvanced <robots@stbaglobal.com>
2026-06-12 16:10:36 -0500

    added fancy clock

--- teacher/fancy_clock.py
+++ teacher/fancy_clock.py
@@ -10,36 +10,32 @@ def main():
     LCD1602.init()
     
     while True:
+        LCD1602.clear()
+        now = datetime.now()
         if switch.is_pressed:
             disp_mode = disp_mode + 1
             if disp_mode > date_mode:
                 disp_mode = time_mode
         if disp_mode == time_mode:
-            display_time()
+            display_time(now)
         elif disp_mode == seconds_mode:
-            display_seconds()
+            display_seconds(now)
         elif disp_mode == date_mode:
-            display_date()
+            display_date(now)
         time.sleep(0.5)
        
 
-def display_time():
-    LCD1602.clear()
-    now = datetime.now()
+def display_time(now):
     current_time = now.strftime("%H:%M:%S")
     LCD1602.write(0, 0, 'Time:')
     LCD1602.write(1, 1, current_time)
 
-def display_seconds():
-    LCD1602.clear()
-    now = datetime.now()
+def display_seconds(now):
     current_seconds = now.strftime("  %S")
     LCD1602.write(0, 0, 'Seconds:')
     LCD1602.write(1, 1, current_seconds)
 
-def display_date():
-    LCD1602.clear()
-    now = datetime.now()
+def display_date(now):
     current_date = now.strftime("%m%d")
     LCD1602.write(0, 0, 'Date:')
     LCD1602.write(1, 1, current_date)
